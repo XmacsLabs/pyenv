70f7224ae68a78cd7cecd7c0c70472217655ca9b
沈达 <sadhen@zoho.com>
2021-05-23 14:09:26 +0800

    Initial workable version

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Enhanced Python Plugin with pyenv Support for GNU TeXmacs
+## Installation
+```
+# For GNU/Linux or macOS
+git clone https://gitee.com/sadhen/pyenv.git $HOME/.TeXmacs/plugins/pyenv
+
+# For Windows
+git clone https://gitee.com/sadhen/pyenv.git %APPDATA%\TeXmacs\plugins\pyenv
+```
+
+Installed under `TEXMACS_HOME_PATH`, this enhanced python plugin will
+override the system default one.
+
---
+++ progs/init-pyenv.scm
@@ -0,0 +1,38 @@
+(define (python-serialize lan t)
+  (with u (pre-serialize lan t)
+    (with s (texmacs->code (stree->tree u) "SourceCode")
+      (string-append  s  "\n<EOF>\n"))))
+
+(define (python-entry)
+  (if (url-exists? "$TEXMACS_HOME_PATH/plugins/tmpy")
+      (string-append (getenv "TEXMACS_HOME_PATH")
+                     "/plugins/tmpy/session/tm_python.py")
+      (string-append (getenv "TEXMACS_PATH")
+                     "/plugins/tmpy/session/tm_python.py")))
+
+(define (pyenv-root) (string-append (getenv "HOME") "/.pyenv"))
+
+(define (pyenv-virtualenvs) 
+  (string-split
+   (var-eval-system "pyenv virtualenvs --bare --skip-aliases") #\nl))
+
+(define (pyenv-which-python env)
+  (string-append (pyenv-root) "/versions/" env "/bin/python"))
+  
+(define (pyenv-launcher env)
+  (string-append (pyenv-which-python env) " " (python-entry)))
+
+(define (other-pyenv-launchers)
+  (map (lambda (name) (list :launch name (pyenv-launcher name))) (pyenv-virtualenvs)))
+
+(define (pyenv-launchers)
+  (cons (list :launch (string-append "python" (python-entry)))
+    (other-pyenv-launchers)))
+
+(plugin-configure python 
+  (:require (url-exists-in-path? "pyenv"))
+  (:versions (pyenv-virtualenvs))
+  ,@(pyenv-launchers)
+  (:serializer ,python-serialize)
+  (:session "Python")
+  (:scripts "Python"))
